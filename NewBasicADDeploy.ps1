ddd115d5769ac4accc83a495314163cf064c76ea
GitHargie <149028192+GitHargie@users.noreply.github.com>
2025-03-22 03:58:30 +0800

    Update NewBasicADDeploy.ps1
    Change dn2 value from 8.8.8.8 to 127.0.0.1

--- NewBasicADDeploy.ps1
+++ NewBasicADDeploy.ps1
@@ -24,7 +24,7 @@ $formode = "WinThreshold"												#Specifies the forest functional level for 
 $cred = Get-Credential
 $pass = $cred.Password  
 $dns1 = "192.168.100.105"	 											#Replace the string of the actual primary dns address.
-$dns2 = "8.8.8.8"			 											#Replace the string of the actual secondary dns address.
+$dns2 = "12.0.0.1"			 											#Replace the string of the actual secondary dns address.
 $netinterface = "Ethernet" 												#Replace the string of the actual name of your network interface. In this one, we assume that the network interface name has a string of "Ethernet".
 
 # 2. Install the Active Directory Role:
